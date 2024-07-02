4abe8f5b2cdcf4047df28764c011595c20d2b774
Valentin <vrmm0512@gmail.com>
2024-07-02 17:37:16 -0400

    Crud n2

--- src/main/java/com/pandadescuentos/spring/controller/BancoController.java
+++ src/main/java/com/pandadescuentos/spring/controller/BancoController.java
@@ -6,12 +6,9 @@ import com.pandadescuentos.spring.model.Banco;
 import com.pandadescuentos.spring.service.BancoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Import(WebConfig.class)
 @RestController
@@ -21,8 +18,22 @@ public class BancoController {
     @Autowired
     private BancoService bancoService;
 
-    @GetMapping
-    public List<Banco> obtenerBancos() {
-        return bancoService.obtenerTodosLosBancos();
+    @GetMapping("/{id}")
+    public Banco obtenerBancoPorId(@PathVariable Long id) {
+        var bancoSolicitado = bancoService.findById(id);
+        return bancoSolicitado;
+    }
+
+    @PostMapping ("/agregar")
+    public Banco addBanco(@RequestBody Banco banco){  // el @RequestBody indica que se entregara un objeto en el cuerpo de la peticion
+        Banco bancoAgregado = bancoService.agregarBanco(banco);
+        return bancoAgregado;
+
+    }
+
+    @DeleteMapping("/eliminar")
+    public ResponseEntity<String> eliminarBanco(@RequestParam Long id){
+        bancoService.eliminarBanco(id);
+        return new ResponseEntity<>("Eliminado exitosamente", HttpStatus.OK);
     }
 }
